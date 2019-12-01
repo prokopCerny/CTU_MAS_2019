d985c3408a616f5b4569478988bc6bb05ea45727
Prokop Cerny <5815801+prokopCerny@users.noreply.github.com>
2019-12-01 18:45:13 +0100

    SEM02 solved LP problem for EFG tree game

--- Sem02/game_lp.py
+++ Sem02/game_lp.py
@@ -32,10 +32,10 @@ from game_tree import *
 #
 # At the course webpage, we have calculated some testing game values for you.
 # You can use them to check if your LP has been well specified.
-import cvxopt
 import numpy as np
+from cvxopt import matrix, solvers
 
-class Node:
+class MyNode:
     def __init__(self, val: float, type: HistoryType, player: int, p1seq, p2seq, infoset: int):
         self.infoset = infoset
         self.p2seq = p2seq
@@ -80,12 +80,12 @@ def transform_tree(root:History):
         value: float = float(chance if root.type() != HistoryType.terminal else chance * root.utility())
         cur_player = int(root.current_player()) if root.type() == HistoryType.decision else -1
 
-        node = Node(value,
-                    root.type(),
-                    cur_player,
-                    p1seq,
-                    p2seq,
-                    infoset)
+        node = MyNode(value,
+                      root.type(),
+                      cur_player,
+                      p1seq,
+                      p2seq,
+                      infoset)
 
         if root.type() == HistoryType.terminal:
             return node
@@ -100,7 +100,7 @@ def transform_tree(root:History):
 
     return new_root, p1seq_to_index, p2seq_to_index, p1info_to_index, p2info_to_index
 
-def create_matrices(root: Node, player: int, p_seq_to_index, op_info_to_index, p_info_to_index, op_seq_to_index):
+def create_matrices(root: MyNode, player: int, p_seq_to_index, op_info_to_index, p_info_to_index, op_seq_to_index):
     p_info_count = len(p_info_to_index)
     p_seq_count = len(p_seq_to_index)
     op_info_count = len(op_info_to_index)
@@ -110,23 +110,48 @@ def create_matrices(root: Node, player: int, p_seq_to_index, op_info_to_index, p
     b_len = p_info_count+1
     G1_dimensions = (2*(p_seq_count-1), p_seq_count + op_info_count)
     h1_len = 2*(p_seq_count-1)
-    G_dimensions = (op_seq_count - 1, p_seq_count + op_info_count)
-    h_len = op_seq_count - 1
+    Gp_dimensions = (op_seq_count - 1, p_seq_count + op_info_count)
+    hp_len = op_seq_count - 1
+    c_len = p_seq_count + op_info_count
 
     op_info_shift = {info: index + p_seq_count for (info, index) in op_info_to_index.items()}
     op_seq_shift = {seq: index-1 for (seq, index) in op_seq_to_index.items()}
 
     A = np.zeros(A_dimensions)
-    G1 = np.zeros(G1_dimensions)
-    G = np.zeros(G_dimensions)
+    Gp = np.zeros(Gp_dimensions)
+    c = np.zeros(c_len)
 
-    def handle_node(node: Node):
+    def handle_opponent_child(node: MyNode, infoset: int):
+        if node.type == HistoryType.terminal:
+            p_seq = node.p1seq if player == 0 else node.p2seq
+            op_seq = node.p1seq if player == 1 else node.p2seq
+            Gp[op_seq_shift[op_seq], p_seq_to_index[p_seq]] = -node.val
+            Gp[op_seq_shift[op_seq], op_info_shift[infoset]] = 1
+        elif node.type == HistoryType.chance:
+            for child in node.children:
+                handle_opponent_child(child, infoset)
+        else:
+            if node.player != player:
+                op_seq = node.p1seq if player == 1 else node.p2seq
+                Gp[op_seq_shift[op_seq], op_info_shift[infoset]] = 1
+                Gp[op_seq_shift[op_seq], op_info_shift[node.infoset]] = -1
+            else:
+                for child in node.children:
+                    handle_opponent_child(child, infoset)
+
+    def handle_node(node: MyNode):
         if node.player == player:
             cur_seq = node.p1seq if player == 0 else node.p2seq
             for child in node.children:
                 child_seq = child.p1seq if player == 0 else child.p2seq
                 A[p_info_to_index[node.infoset], p_seq_to_index[child_seq]] = 1
                 A[p_info_to_index[node.infoset], p_seq_to_index[cur_seq]] = -1
+        elif 0 <= node.player < 2:
+            cur_seq = node.p1seq if node.player == 0 else node.p2seq
+            if cur_seq == ("",):
+                c[op_info_shift[node.infoset]] = -1
+            for child in node.children:
+                handle_opponent_child(child, node.infoset)
 
         for child in node.children:
             handle_node(child)
@@ -138,13 +163,20 @@ def create_matrices(root: Node, player: int, p_seq_to_index, op_info_to_index, p
     G1 = np.c_[np.zeros(h1_len), G1, np.zeros((h1_len, op_info_count))]
     tmp = np.array([1, 0])
     h1 = np.kron(np.ones(p_seq_count-1), tmp)
+    if player == 1:
+        c = -c
+        Gp = -Gp
+    hp = np.zeros(hp_len)
+    G = np.r_[G1, Gp]
+    h = np.concatenate([h1, hp])
+
 
 
     A[-1, 0] = 1
     b = np.zeros(b_len)
     b[-1] = 1
 
-    return A, b
+    return A, b, G, h, c
 
 
 def game_value(root: History, player: Player) -> float:
@@ -166,7 +198,18 @@ def game_value(root: History, player: Player) -> float:
                    second player has index 1
     :return: expected value in the root for given player
     """
-    raise NotImplementedError
+    new_root, p1seq_to_index, p2seq_to_index, p1info_to_index, p2info_to_index = transform_tree(root)
+    p_sti = p1seq_to_index if int(player) == 0 else p2seq_to_index
+    p_iti = p1info_to_index if int(player) == 0 else p2info_to_index
+    op_sti = p1seq_to_index if int(player) == 1 else p2seq_to_index
+    op_iti = p1info_to_index if int(player) == 1 else p2info_to_index
+    A, b, G, h, c = create_matrices(new_root, player, p_sti, op_iti, p_iti, op_sti)
+    Aopt, bopt, Gopt, hopt, copt = matrix(A), matrix(b), matrix(G), matrix(h), matrix(c)
+    solvers.options['show_progress'] = False
+    sol = solvers.lp(c=copt, G=Gopt, h=hopt, A=Aopt, b=bopt)
+    x = np.squeeze(np.array(sol['x']))
+    val = np.dot(c, x)
+    return -val
 
 
 ########### Do not modify code below.
@@ -176,5 +219,6 @@ if __name__ == '__main__':
     root_history = create_root()
     # additionally specify for which player it should be solved
     player = int(input())
+    # player = 0
 
-    #print(game_value(root_history, player))
+    print(game_value(root_history, player))
